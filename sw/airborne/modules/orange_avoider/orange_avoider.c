72f2a796e2884ee28f05bb77d4e7d3c7d4d5f4aa
Achin Agarwal <aagarwal@tcd.ie>
2019-03-28 17:40:52 +0100

    change periodic freq of orange avoider.xml

--- sw/airborne/modules/orange_avoider/orange_avoider.c
+++ sw/airborne/modules/orange_avoider/orange_avoider.c
@@ -78,7 +78,7 @@ enum navigation_state_t navigation_state = SEARCH_FOR_SAFE_HEADING;
 int32_t color_count = 0;               // orange color count from color filter for obstacle detection
 int16_t obstacle_free_confidence = 0;   // a measure of how certain we are that the way ahead is safe.
 float heading_increment = 5.0f;          // heading angle increment [deg]
-float heading_increment_home = 180.0f;	 // Execute 180 [deg] upon encountering boundary
+//float heading_increment_home = 180.0f;	 // Execute 180 [deg] upon encountering boundary
 float maxDistance = 2.25;               // max waypoint displacement [m] 2.25 initially
 
 /*
@@ -158,9 +158,9 @@ void orange_avoider_periodic(void)
       break;
     case OBSTACLE_FOUND:
       // stop
-      //waypoint_set_here_2d(WP_GOAL);
-      //waypoint_set_here_2d(WP_TRAJECTORY);
-      moveWaypointForward(WP_TRAJECTORY,2.0f * moveDistance); //additional line added
+      waypoint_set_here_2d(WP_GOAL);
+      waypoint_set_here_2d(WP_TRAJECTORY);
+      //moveWaypointForward(WP_TRAJECTORY,0.5f * moveDistance); //additional line added
 
       // randomly select new search direction
       chooseRandomIncrementAvoidance();
